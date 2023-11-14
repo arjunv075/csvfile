f506ac4972555dfbfed1b33781b74f2f30edffe4
arjunv075 <arjunv075@gmail.com>
2023-11-14 17:42:20 +0530

    add generate_qr_code() to id.py program.

--- id.py
+++ id.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import requests
 
 
 def read_csv(file_path):
@@ -28,6 +29,22 @@ END:VCARD
     return vcard_template.strip()
 
 
+def generate_qr_code(data, output_directory):
+    os.makedirs(output_directory, exist_ok=True)
+
+    for entry in data:
+        first_name, last_name, title, email, phone = entry
+        vcard = generate_vcard_template(first_name, last_name, title, email, phone)
+
+        qr_code_url = f"https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl={vcard}"
+
+        response = requests.get(qr_code_url)
+        qr_code_image_path = os.path.join(output_directory, f"{email.lower()}_qr_code.png")
+
+        with open(qr_code_image_path, "wb") as qr_code_file:
+            qr_code_file.write(response.content)
+
+
 def generate_vcard(data, output_directory):
     os.makedirs(output_directory, exist_ok=True)
 
@@ -51,6 +68,8 @@ def main():
     data = read_csv(csv_file_path)
 
     generate_vcard(data, output_directory)
+
+    generate_qr_code(data, output_directory)
 
 
 if __name__ == "__main__":
