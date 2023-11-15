51976a02d6b7744c9893815b16c1566602d1c1ca
arjunv075 <arjunv075@gmail.com>
2023-11-15 18:17:44 +0530

    add logging to id.py and spec.txt file.
    add logging module to the program to generate vcard file and qr code.
    add spec.txt file also for the id.py file.

--- id.py
+++ id.py
@@ -1,16 +1,38 @@
+import argparse
+import logging
 import csv
 import os
 import requests
 
+logger = None
 
-def read_csv(file_path):
+def parse_args():
+    parser = argparse.ArgumentParser(prog="id.py", description="Generates log files of generated vcards and QR codes")
+    parser.add_argument("opfile", help="Name of input CSV file")
+    parser.add_argument("-v", "--verbose", help="Print detailed logging", action='store_true', default=False)
+    args = parser.parse_args()
+    return args
+
+def setup_logging(log_level):
+    global logger
+    logger = logging.getLogger("SheetGen")
+    handler = logging.StreamHandler()
+    fhandler = logging.FileHandler("run.log")
+    fhandler.setLevel(logging.DEBUG)
+    handler.setLevel(log_level)
+    handler.setFormatter(logging.Formatter("[%(levelname)s] %(asctime)s | %(filename)s:%(lineno)d | %(message)s"))
+    fhandler.setFormatter(logging.Formatter("[%(levelname)s] %(asctime)s | %(filename)s:%(lineno)d | %(message)s"))
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+    logger.addHandler(fhandler)
+
+def read_csv(csv_file_path):
     data = []
-    with open(file_path, "r") as csvfile:
+    with open(csv_file_path, "r") as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             data.append(row)
     return data
-
 
 def generate_vcard_template(fname, lname, title, email, phone):
     vcard_template = f"""
@@ -28,49 +50,57 @@ END:VCARD
 """
     return vcard_template.strip()
 
-
-def generate_qr_code(data, output_directory):
-    os.makedirs(output_directory, exist_ok=True)
+def generate_qr_code(data):
+    os.makedirs("./vcf_files", exist_ok=True)
+    global count
+    count = 1
 
     for entry in data:
+        logger.debug("writing row %d", count)
+        count += 1
         first_name, last_name, title, email, phone = entry
         vcard = generate_vcard_template(first_name, last_name, title, email, phone)
 
         qr_code_url = f"https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl={vcard}"
 
         response = requests.get(qr_code_url)
-        qr_code_image_path = os.path.join(output_directory, f"{email.lower()}_qr_code.png")
+        qr_code_image_path = os.path.join("./vcf_files", f"{email.lower()}_qr_code.png")
 
         with open(qr_code_image_path, "wb") as qr_code_file:
             qr_code_file.write(response.content)
+    logger.info("done")
 
+def generate_vcard(csv_file_path):
+    os.makedirs("./vcf_files", exist_ok=True)
+    global count
+    count = 1
 
-def generate_vcard(data, output_directory):
-    os.makedirs(output_directory, exist_ok=True)
+    data = read_csv(csv_file_path)
 
     for entry in data:
+        logger.debug("writing row %d", count)
+        count += 1
         first_name, last_name, title, email, phone = entry
         vcard = generate_vcard_template(first_name, last_name, title, email, phone)
 
         file_name = f"{email.lower()}_card.vcf"
-        file_path = os.path.join(output_directory, file_name)
+        file_path = os.path.join("./vcf_files", file_name)
 
         with open(file_path, "w") as vcard_file:
             vcard_file.write(vcard)
 
-    print("created vcard files")
-
+    logger.info("created vcard files")
 
 def main():
-    csv_file_path = "names.csv"
-    output_directory = "./vcf_files"
+    args = parse_args()
 
-    data = read_csv(csv_file_path)
+    if args.verbose:
+        setup_logging(logging.DEBUG)
+    else:
+        setup_logging(logging.INFO)
 
-    generate_vcard(data, output_directory)
-
-    generate_qr_code(data, output_directory)
-
+    generate_vcard(args.opfile)
+    generate_qr_code(read_csv(args.opfile))
 
 if __name__ == "__main__":
     main()
