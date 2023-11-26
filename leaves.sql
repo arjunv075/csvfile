6cdac2b623b1449c827c753cba6ceb412a672e8a
arjunv075 <arjunv075@gmail.com>
2023-11-26 20:10:14 +0530

    add functionality to add leaves to vcf.py

---
+++ leaves.sql
@@ -0,0 +1,2 @@
+INSERT INTO leaves (date,employee_id,reason) VALUES ('2023-11-13',4,'cough'),('2023-11-08',4,'cold');
+INSERT INTO leaves (date,employee_id,reason) VALUES ('2023-11-01',3,'headache');
