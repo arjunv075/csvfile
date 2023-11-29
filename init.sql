2d02e6ff42805c305b9c2826f902501dc6cc1621
arjunv075 <arjunv075@gmail.com>
2023-11-29 17:08:28 +0530

    add modified init file

--- init.sql
+++ init.sql
@@ -1,16 +1,13 @@
-CREATE TABLE employees(
-       id SERIAL PRIMARY KEY,
-       fname VARCHAR(50),
-       lname VARCHAR(50),
-       designation VARCHAR(100),
-       email VARCHAR(120),
-       phone VARCHAR(50)
-       );
+CREATE TABLE details (serial_number SERIAL PRIMARY KEY, lastname VARCHAR(50),firstname VARCHAR(50),title VARCHAR(200),email VARCHAR(150),phone_number VARCHAR(50));
 
-CREATE TABLE leaves (
-       id SERIAL PRIMARY KEY,
-       date DATE,
-       employee INTEGER REFERENCES employees(id),
-       reason VARCHAR(200),
-       UNIQUE (employee, date) -- An employee can take only one leave on a given day
-       );
+CREATE TABLE leaves (serial_number SERIAL PRIMARY KEY,date DATE NOT NULL,employee_id INTEGER NOT NULL REFERENCES details(serial_number),reason VARCHAR(50) NOT NULL,UNIQUE (date,employee_id));
+
+CREATE TABLE designation (id SERIAL,designation VARCHAR(150) PRIMARY KEY,num_of_leaves INTEGER,UNIQUE (id,designation));
+
+
+INSERT INTO designation (designation,num_of_leaves) VALUES ('Staff Engineer',20);
+INSERT INTO designation (designation,num_of_leaves) VALUES ('Senior Engineer',18);
+INSERT INTO designation (designation,num_of_leaves) VALUES ('Junior Engineer',12);
+INSERT INTO designation (designation,num_of_leaves) VALUES ('Technical Lead',12);
+INSERT INTO designation (designation,num_of_leaves) VALUES ('Project Manager',15);
+
