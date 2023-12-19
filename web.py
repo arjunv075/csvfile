2ae80bead516bc50ee22cefd7b63e6ab0d853590
arjunv075 <arjunv075@gmail.com>
2023-12-19 17:12:59 +0530

    change the functions to return JSON format

--- web.py
+++ web.py
@@ -1,11 +1,12 @@
 import flask
 from flask import render_template,request,redirect,url_for
-
+from flask_cors import CORS
 import models
 from sqlalchemy import func
 
 
 app = flask.Flask("hrms")
+CORS(app)
 db = models.SQLAlchemy(model_class = models.HRDBBase)
 
 @app.route("/")
@@ -18,13 +19,16 @@ def index():
 def employees():
     employees = db.select(models.Employee).order_by(models.Employee.firstname)
     users = db.session.execute(employees).scalars()
-    ret ={}
+    ret =[]
     for user in users:
-       ret[user.empid]= {"firstname" : user.firstname,
+       emp_list= {
+                      "id": user.empid,
+                      "firstname" : user.firstname,
                       "lastname" : user.lastname,
                       "title" : user.title.title,
                       "email" : user.email,
                       "phone" : user.ph_no}
+       ret.append(emp_list)
     return flask.jsonify(ret)
 
   
@@ -36,7 +40,8 @@ def employee_details(empid):
   leaves = db.session.execute(emp_count).scalar()
   leave_count = db.select(models.Designation.max_leaves).where(models.Designation.jobid == models.Employee.title_id, models.Employee.empid==empid)
   max_leaves = db.session.execute(leave_count).scalar()
-  ret = {"employee_id" : user.empid,
+  ret=[]
+  emp_details = {"employee_id" : user.empid,
          "firstname" : user.firstname,
          "lastname" : user.lastname,
          "title" : user.title.title,
@@ -45,6 +50,7 @@ def employee_details(empid):
          "leaves": leaves,
          "max_leaves" : max_leaves
   }
+  ret.append(emp_details)
   return flask.jsonify(ret)
 
 @app.errorhandler(500)   
@@ -58,15 +64,17 @@ def page_not_found(error):
 
 @app.route("/leave/<int:empid>", methods=["GET", "POST"])
 def addleave(empid):
-  employees = db.select(models.Employee).where(models.Employee.empid == empid)
-  user = db.session.execute(employees).scalar()
   if request.method == "POST":
-    date = request.form['date']
-    reason = request.form['reason'] 
-    enter_data = models.Leaves(empid=empid ,date=date, reason=reason)
-    db.session.add(enter_data)
+    data = request.get_json()
+    date = data.get('date')
+    reason = data.get('reason')
+    if not date or not reason:
+       return flask.jsonify({'error': 'Enter data'}), 400
+    insert_data = models.Leaves(empid=empid ,date=date, reason=reason)
+    db.session.add(insert_data)
     db.session.commit()
-    return redirect(url_for("employees"))
+    return flask.jsonify({'message': 'Leave Posted'}), 200
+  return flask.jsonify({'error': 'Function Not Allowed'}), 405
   
 @app.route("/about")
 def about():
